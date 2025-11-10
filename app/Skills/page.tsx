614e587006dbd39bd74c76113401b2e9e2674cdb
Cyberbee-pro <Cyberbeepro@gmail.com>
2025-11-10 15:01:36 +0530

    Skills dev startedd

--- app/Skills/page.tsx
+++ app/Skills/page.tsx
@@ -3,26 +3,46 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="Main">
-      <div className="fixed w-full bg-black h-17 text-2xl"> {/*Navigation bar */}
+    <div className="flex flex-col min-h-screen">
+      <Navigation />
+      <Content />
+    </div>
+  );
+}
+
+const Navigation =() =>{
+  return<div className="fixed w-full bg-black h-17 text-2xl z-60"> {/*Navigation bar */}
         <div className="flex mt-5">
-          <div className="flex flex-start ml-5 pr-220">
-            <Link href={"../"} className="pl-4 pr-4 mr-4">Mee</Link>
-          </div>
+          <Link href={"../"} className="flex flex-start ml-5 pr-225">
+            Mee
+          </Link>
           <div className="flex right-0">
-            <Link href={"/Skills"} className="pl-4 pr-4 mr-4">Skills</Link>
+            <div className="pl-4 pr-4 mr-4">Skills</div>
             <Link href={"/Resume"} className="pl-4 pr-4 mr-4">Resume</Link>
             <Link href={"/Portfolio"} className="pl-4 pr-4 mr-4">Portfolio</Link>
             <Link href={"/Contact_me"} className="pl-4 mr-3">Contact Me</Link>
           </div>
         </div>
-      </div>  {/*Navigation bar end */}
+      </div>
+}
 
+const Content =() =>{
+  return<div className="absolute inset-x-0 top-16 h-16">
+          <div className="tracking-widest text-[128px] p-3 pl-11">Skills</div>
+            <div className="flex tracking-wider text-[26px] p-2 pl-11">
+            <ul className="flex space-x-6 list-disc pl-3">
+              <li className="p-3">FullStack Dev</li>
+              <li className="p-3">UI/UX Designer</li>
+              <li className="p-3">Game Devoloper</li>
+              <li className="p-3">Ai Engineer</li>
+            </ul>
+            </div>
+          <div className="flex text-[45px] p-2 pl-11"><div className="bg-[url(/code.svg)] size-5"></div>Programing Languages</div>
+        </div>
+};
 
-
-
-      <footer> {/*Footer section*/}
-      <div className="absolute bg-black w-full inset-x-0 bottom-0 p-10" >
+const Footer =() => {
+  return<div className="bg-black w-full p-10 mt-auto" >
         <div className="grid grid-rows-4 grid-cols-2">
           <div>
             Mee
@@ -53,7 +73,4 @@ export default function Home() {
           </div>
         </div>
       </div>
-      </footer> {/*Footer section end*/}
-    </div>
-  );
-}
+};
